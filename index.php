4b3add830e38adf4a8664d2d50174a912759a3db
Zulfiana Rahmi <zulfianarahmi4@gmail.com>
2024-09-15 13:35:49 +0700

    menambahkan index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <p>Hello word</p>
+    <p>Zulfiana rahmi</p>
+</body>
+</html>
